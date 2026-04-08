20cd3c8931ce08deb4551ed118a704be7e0e1034
Uvanesh Waran <uvanesh.waran@keurodotlife.onmicrosoft.com>
2026-04-08 14:54:40 +0530

    Added budget recommendation feature

--- api.py
+++ api.py
@@ -1,8 +1,4 @@
-from model import predict_performance
-
-def get_prediction():
-    return predict_performance(200, 5000)
-
-
-if __name__ == "__main__":
-    print(get_prediction())
+def recommend_budget(ctr):
+    if ctr > 0.1:
+        return "Increase Budget"
+    return "Decrease Budget"
